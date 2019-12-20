7abe53adc066f5bedde23941fc6365e4f88edaef
Jordan Harband <ljharb@gmail.com>
2019-12-20 11:14:25 -0800

    [Refactor] use split-up `es-abstract` (72% bundle size decrease)

--- implementation.js
+++ implementation.js
@@ -1,27 +1,34 @@
 'use strict';
 
-var ES = require('es-abstract/es2019');
-var bind = require('function-bind');
+var Call = require('es-abstract/2019/Call');
+var Get = require('es-abstract/2019/Get');
+var HasProperty = require('es-abstract/2019/HasProperty');
+var IsCallable = require('es-abstract/2019/IsCallable');
+var ToObject = require('es-abstract/2019/ToObject');
+var ToString = require('es-abstract/2019/ToString');
+var ToUint32 = require('es-abstract/2019/ToUint32');
+var callBound = require('es-abstract/helpers/callBound');
 var isString = require('is-string');
 
 // Check failure of by-index access of string characters (IE < 9) and failure of `0 in boxedString` (Rhino)
 var boxedString = Object('a');
 var splitString = boxedString[0] !== 'a' || !(0 in boxedString);
 
-var strSplit = bind.call(Function.call, String.prototype.split);
+var $split = callBound('String.prototype.split');
+var $TypeError = TypeError;
 
 module.exports = function reduceRight(callbackfn) {
-	var O = ES.ToObject(this);
-	var self = splitString && isString(O) ? strSplit(O, '') : O;
-	var len = ES.ToUint32(ES.Get(self, 'length'));
+	var O = ToObject(this);
+	var self = splitString && isString(O) ? $split(O, '') : O;
+	var len = ToUint32(Get(self, 'length'));
 
 	// If no callback function or if callback is not a callable function
-	if (!ES.IsCallable(callbackfn)) {
-		throw new TypeError('Array.prototype.reduceRight callback must be a function');
+	if (!IsCallable(callbackfn)) {
+		throw new $TypeError('Array.prototype.reduceRight callback must be a function');
 	}
 
 	if (len === 0 && arguments.length < 2) {
-		throw new TypeError('reduceRight of empty array with no initial value');
+		throw new $TypeError('reduceRight of empty array with no initial value');
 	}
 
 	var k = len - 1;
@@ -33,21 +40,21 @@ module.exports = function reduceRight(callbackfn) {
 	} else {
 		kPresent = false;
 		while (!kPresent && k >= 0) {
-			Pk = ES.ToString(k);
-			kPresent = ES.HasProperty(O, Pk);
+			Pk = ToString(k);
+			kPresent = HasProperty(O, Pk);
 			if (kPresent) {
-				accumulator = ES.Get(O, Pk);
+				accumulator = Get(O, Pk);
 			}
 			k -= 1;
 		}
 	}
 
 	while (k >= 0) {
-		Pk = ES.ToString(k);
-		kPresent = ES.HasProperty(O, Pk);
+		Pk = ToString(k);
+		kPresent = HasProperty(O, Pk);
 		if (kPresent) {
-			var kValue = ES.Get(O, Pk);
-			accumulator = ES.Call(callbackfn, void undefined, [accumulator, kValue, k, O]);
+			var kValue = Get(O, Pk);
+			accumulator = Call(callbackfn, void undefined, [accumulator, kValue, k, O]);
 		}
 		k -= 1;
 	}
